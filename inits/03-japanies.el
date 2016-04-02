5e938021ee5320d193cef8f93ec627bfad4d2ae5
llby <llby@shingor.net>
2016-04-02 21:41:27 +0900

    ddd 03-japanies.el

---
+++ inits/03-japanies.el
@@ -0,0 +1,16 @@
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+
+;;(if window-system
+;;    (progn
+      ;; フォント
+      ;; (pp (font-family-list)) でfont-familyの出力可能
+;;      (set-face-attribute 'default nil
+;;                          :height 120 :family "Menlo"
+;;                          )
+;;      (set-fontset-font nil 'japanese-jisx0208
+;;                        (font-spec :family "Noto Sans Japanese"))
