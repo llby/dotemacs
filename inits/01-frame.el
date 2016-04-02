22237304c2521e1a1cf0d6b0c067b651b0fb7cc1
llby <llby@shingor.net>
2016-04-02 21:23:44 +0900

    add 01-frame.el

---
+++ inits/01-frame.el
@@ -0,0 +1,16 @@
+(setq default-frame-alist
+      (append '((width                . 85)  ; フレーム幅
+                (height               . 38 ) ; フレーム高
+             ;; (left                 . 70 ) ; 配置左位置
+             ;; (top                  . 28 ) ; 配置上位置
+                (line-spacing         . 0  ) ; 文字間隔
+                (left-fringe          . 10 ) ; 左フリンジ幅
+                (right-fringe         . 11 ) ; 右フリンジ幅
+                (menu-bar-lines       . 1  ) ; メニューバー
+                (tool-bar-lines       . 1  ) ; ツールバー
+                (vertical-scroll-bars . 1  ) ; スクロールバー
+                (scroll-bar-width     . 17 ) ; スクロールバー幅
+                (cursor-type          . box) ; カーソル種別
+                (alpha                . 100) ; 透明度
+                ) default-frame-alist) )
+(setq initial-frame-alist default-frame-alist)
